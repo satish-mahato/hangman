8ce8c071337e7486cfb1acb4a21a78307d4ec3c3
satish7654 <satishsingh202dd+@gmail.com>
2024-10-13 16:35:02 +0545

    second

--- src/components/TextInputForm/TextInputForm.jsx
+++ src/components/TextInputForm/TextInputForm.jsx
@@ -1,18 +1,28 @@
 import Button from "../Button/Button";
 import TextInput from "../TextInput/TextInput";
 
-function TextInputForm({ handleFormSubmit, handleTextInputForm }) {
+function TextInputForm({
+  handleFormSubmit,
+  handleTextInputForm,
+  handleShowHideClick,
+  inputType,
+}) {
   return (
     <form onSubmit={handleFormSubmit}>
       <div>
         <TextInput
+          type={inputType}
           label={"Enter Your Word / Pharse"}
           placeholder="Enter Your word/ pharse"
           onChangeHandler={handleTextInputForm}
         />
       </div>
       <div>
-        <Button text={"Show/Hide"} styleType="warning" />
+        <Button
+          text={inputType === "password" ? "Show" : "Hide"}
+          styleType="warning"
+          onClickHandler={handleShowHideClick}
+        />
       </div>
       <div>
         <Button styleType="success" text={"submit"} type="submit" />
--- src/components/TextInputForm/TextInputFormContainer.jsx
+++ src/components/TextInputForm/TextInputFormContainer.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import TextInputForm from "./TextInputForm";
 
 function TextInputFormContainer() {
+  const [inputType, setInputType] = useState("password");
   function handleFormSubmit(event) {
     event.preventDefault();
     console.log("Form Submited");
@@ -10,10 +12,21 @@ function TextInputFormContainer() {
     console.log("Input Text change");
     console.log(event.target.value);
   }
+  function handleShowHideClick() {
+    console.log("Show/Hide Button clicked");
+    if (inputType === "password") {
+      setInputType("text");
+    } else {
+      setInputType("password");
+    }
+    console.log(inputType);
+  }
   return (
     <TextInputForm
+      inputType={inputType}
       handleFormSubmit={handleFormSubmit}
       handleTextInputForm={handleTextInputForm}
+      handleShowHideClick={handleShowHideClick}
     />
   );
 }
