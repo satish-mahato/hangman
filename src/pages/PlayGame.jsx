4c3c1421da5a0780a6de5618ec99b99d2e363175
satish7654 <satishsingh202dd+@gmail.com>
2024-10-14 22:35:58 +0545

    fifth

--- src/pages/PlayGame.jsx
+++ src/pages/PlayGame.jsx
@@ -1,14 +1,26 @@
 import { Link, useLocation } from "react-router-dom";
 import MaskedText from "../components/MaskedText/MaskedText";
+import LetterButton from "../components/LetterButton/LetterButton";
+import { useState } from "react";
 
 function PlayGame() {
   const { state } = useLocation();
-
+  const [guessedLetters, SetGuessedLetters] = useState([]);
+  function handleLetterClick(letter) {
+    SetGuessedLetters([...guessedLetters, letter]);
+  }
   return (
     <div>
       <h1>Play Game</h1>
 
-      <MaskedText text={state.wordSelect} guessedLetters={["H", "B"]} />
+      <MaskedText text={state.wordSelect} guessedLetters={guessedLetters} />
+      <div>
+        <LetterButton
+          text={state.wordSelect}
+          guessedLetters={guessedLetters}
+          onLetterClick={handleLetterClick}
+        />
+      </div>
       <Link
         to={"/start"}
         className="text-blue-500 border border-gray-600 rounded-md"
